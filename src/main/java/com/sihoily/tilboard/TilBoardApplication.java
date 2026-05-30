5c424a543d36a6edd54c16df38bed80a7b3af686
siho <dolphin@kakao.com>
2026-05-31 05:13:52 +0900

    first setup

---
+++ src/main/java/com/sihoily/tilboard/TilBoardApplication.java
@@ -0,0 +1,13 @@
+package com.sihoily.tilboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TilBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sihoily/tilboard/TilBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihoily.tilboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
