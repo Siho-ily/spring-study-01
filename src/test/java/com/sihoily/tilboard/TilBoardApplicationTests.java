f26c9de8f6ede6385f03ef08507bb67d176bf599
siho <dolphin@kakao.com>
2026-06-04 23:49:41 +0900

    test: JUnit Platform 런처 추가 및 테스트 환경 설정
    - build.gradle (junit-platform-launcher 추가)
    - src/test/resources/application.yml (H2 설정)
    - TilBoardApplicationTests.java (@Disabled 추가)

--- src/test/java/com/sihoily/tilboard/TilBoardApplicationTests.java
+++ src/test/java/com/sihoily/tilboard/TilBoardApplicationTests.java
@@ -1,8 +1,10 @@
 package com.sihoily.tilboard;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("MemberRepository 구현 전까지 비활성화")
 @SpringBootTest
 class TilBoardApplicationTests {
 
