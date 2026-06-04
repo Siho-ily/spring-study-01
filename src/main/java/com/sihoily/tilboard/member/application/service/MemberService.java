cbeeae289d4a96d7fc336ae7fe67cc1b9bf499bd
siho <dolphin@kakao.com>
2026-06-04 23:49:58 +0900

    feat: MemberService register 구현 및 단위 테스트 추가

---
+++ src/main/java/com/sihoily/tilboard/member/application/service/MemberService.java
@@ -0,0 +1,28 @@
+package com.sihoily.tilboard.member.application.service;
+
+import com.sihoily.tilboard.member.application.port.in.LoginUseCase;
+import com.sihoily.tilboard.member.application.port.in.RegisterMemberUseCase;
+import com.sihoily.tilboard.member.application.port.out.LoadMemberPort;
+import com.sihoily.tilboard.member.application.port.out.SaveMemberPort;
+import com.sihoily.tilboard.member.domain.Member;
+import com.sihoily.tilboard.member.domain.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService implements LoginUseCase, RegisterMemberUseCase {
+    private final LoadMemberPort loadMemberPort;
+    private final SaveMemberPort saveMemberPort;
+
+    @Override
+    public Member login(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public Member register(String email, String password, String nickname) {
+        Member member = new Member(null, email, password, nickname, Role.USER);
+        return saveMemberPort.saveMember(member);
+    }
+}
---
+++ src/test/java/com/sihoily/tilboard/member/application/service/MemberServiceTest.java
@@ -0,0 +1,51 @@
+package com.sihoily.tilboard.member.application.service;
+
+import com.sihoily.tilboard.member.application.port.out.LoadMemberPort;
+import com.sihoily.tilboard.member.application.port.out.SaveMemberPort;
+import com.sihoily.tilboard.member.domain.Member;
+import com.sihoily.tilboard.member.domain.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+    @Mock
+    private SaveMemberPort saveMemberPort;
+    @Mock
+    private LoadMemberPort loadMemberPort;
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void testRegisterSuccess() {
+        // Given - 옳바른 가입 정보, 중복된 데이터가 없을 때
+        String email = "email@example.com";
+        String password = "test1234!";
+        String nickname = "nickname";
+
+        Member savedMember = new Member(1L, email, password, nickname, Role.USER);
+        when(saveMemberPort.saveMember(any())).thenReturn(savedMember);
+
+        // When - 회원 가입을 요청하면
+        Member result = memberService.register(email, password, nickname);
+
+        // Then - 회원가입 성공하고 회원 정보가 반환된다.
+        // 결과 값이 맞는지 검증
+        assertThat(result.email()).isEqualTo(email);
+        assertThat(result.nickname()).isEqualTo(nickname);
+
+        // 실제로 호출 됐는지 검증
+        verify(saveMemberPort).saveMember(any());
+    }
+}
