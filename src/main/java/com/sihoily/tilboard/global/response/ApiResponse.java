59dd464a0764498ef30443b7183caf335e257394
siho <dolphin@kakao.com>
2026-06-10 00:08:11 +0900

    feat: ApiResponse 공통 응답 구조 추가

---
+++ src/main/java/com/sihoily/tilboard/global/response/ApiResponse.java
@@ -0,0 +1,33 @@
+package com.sihoily.tilboard.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    private List<ErrorData> errors;
+
+    public record ErrorData(String type, String field, String message){}
+
+    public static <T> ApiResponse<T> success(T data){
+        return new ApiResponse<>(true, "OK", data, null);
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data){
+        return new ApiResponse<>(true, message, data, null);
+    }
+
+    public static <T> ApiResponse<T> error(String message){
+        return new ApiResponse<>(false, message, null, null);
+    }
+
+    public static <T> ApiResponse<T> error(String message, List<ErrorData> errors){
+        return new ApiResponse<>(false, message, null, errors);
+    }
+}
