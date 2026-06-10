c52a41e8dacc60e58979dbc3b661d16286cd9475
siho <dolphin@kakao.com>
2026-06-10 21:09:49 +0900

    feat: MemberController 추가

---
+++ src/main/java/com/sihoily/tilboard/member/adapter/in/web/MemberController.java
@@ -0,0 +1,35 @@
+package com.sihoily.tilboard.member.adapter.in.web;
+
+import com.sihoily.tilboard.global.response.ApiResponse;
+import com.sihoily.tilboard.member.adapter.in.web.dto.request.SignupRequest;
+import com.sihoily.tilboard.member.adapter.in.web.dto.response.SignupResponse;
+import com.sihoily.tilboard.member.application.port.in.LoginUseCase;
+import com.sihoily.tilboard.member.application.port.in.SignupUseCase;
+import com.sihoily.tilboard.member.domain.Member;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+//@Validated
+public class MemberController {
+    private final LoginUseCase loginUseCase;
+    private final SignupUseCase signupUseCase;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<SignupResponse>> signup(
+            @Valid @RequestBody SignupRequest request
+    ) {
+        Member member = signupUseCase.signup(request.getUserId(), request.getEmail(), request.getPassword(), request.getNickname());
+        SignupResponse response = SignupResponse.from(member);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.success(response));
+    }
+}
