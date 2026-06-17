c38f79772580d54efcd58fe13aa01431776a1298
siho <dolphin@kakao.com>
2026-06-17 21:28:58 +0900

    feat: SignupRequest - 검증 예외 메세지 추가

--- src/main/java/com/sihoily/tilboard/member/adapter/in/web/dto/request/SignupRequest.java
+++ src/main/java/com/sihoily/tilboard/member/adapter/in/web/dto/request/SignupRequest.java
@@ -10,18 +10,18 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignupRequest {
-    @NotBlank
+    @NotBlank(message = "빈 문자열은 허용되지 않습니다.")
     @AlphaNumberic
-    @Size(min = 1, max = 100)
+    @Size(min = 1, max = 100, message = "아이디는 1~100자 이내만 허용됩니다.")
     private String userId;
 
-    @NotBlank
-    @Pattern(regexp = "[a-zA-Z0-9!@#$%^&*()_=+-]+")
-    @Size(min = 8, max = 20)
+    @NotBlank(message = "빈 문자열은 허용되지 않습니다.")
+    @Pattern(regexp = "[a-zA-Z0-9!@#$%^&*()_=+-]+", message = "영·숫자 및 특수문자(!@#$%^&*()_=+-)만 허용됩니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8~20자 이내만 허용됩니다.")
     private String password;
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "빈 문자열은 허용되지 않습니다.")
+    @Email(message = "이메일 형식이 일치하지 않습니다.")
     private String email;
 
     @NotBlank
