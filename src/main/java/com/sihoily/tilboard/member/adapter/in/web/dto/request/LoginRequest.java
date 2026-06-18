b08ca771cbb85b7f67d1964b271643133c17bbbe
siho <dolphin@kakao.com>
2026-06-18 19:58:31 +0900

    feat: LoginRequest DTO 검증 어노테이션 추가

--- src/main/java/com/sihoily/tilboard/member/adapter/in/web/dto/request/LoginRequest.java
+++ src/main/java/com/sihoily/tilboard/member/adapter/in/web/dto/request/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.sihoily.tilboard.member.adapter.in.web.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
+    @NotBlank(message="ID는 빈 값일 수 없습니다.")
     private String userId;
+    @NotBlank(message="PW는 빈 값일 수 없습니다.")
     private String password;
 }
