59a60a26a436db4dd84aef4179b3d74c22215106
siho <dolphin@kakao.com>
2026-06-19 10:43:47 +0900

    fix: RefreshTokenJpaPersistenceAdapter upsert 문제 수정

--- src/main/java/com/sihoily/tilboard/member/adapter/out/persistence/jpa/RefreshTokenJpaEntity.java
+++ src/main/java/com/sihoily/tilboard/member/adapter/out/persistence/jpa/RefreshTokenJpaEntity.java
@@ -1,15 +1,13 @@
 package com.sihoily.tilboard.member.adapter.out.persistence.jpa;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/sihoily/tilboard/member/adapter/out/persistence/jpa/RefreshTokenJpaPersistenceAdapter.java
+++ src/main/java/com/sihoily/tilboard/member/adapter/out/persistence/jpa/RefreshTokenJpaPersistenceAdapter.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,14 +15,16 @@ public class RefreshTokenJpaPersistenceAdapter implements SaveRefreshTokenPort {
     private final RefreshTokenJpaRepository repository;
     @Override
     public void saveRefreshToken(String userId, String refreshToken, LocalDateTime expiration) {
-        if (repository.existsByUserId(userId)) repository.deleteByUserId(userId);
-
-        RefreshTokenJpaEntity entity = RefreshTokenJpaEntity
-                .builder()
+        Optional<RefreshTokenJpaEntity> optionalEntity = repository.findByUserId(userId);
+        RefreshTokenJpaEntity entity = optionalEntity.orElseGet(() -> RefreshTokenJpaEntity.builder()
                 .userId(userId)
                 .refreshToken(refreshToken)
                 .expiresAt(expiration)
-                .build();
+                .build());
+
+        entity.setUserId(userId);
+        entity.setRefreshToken(refreshToken);
+        entity.setExpiresAt(expiration);
 
         repository.save(entity);
     }
